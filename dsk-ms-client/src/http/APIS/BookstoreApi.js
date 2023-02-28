3d755d57b6d88717e6a5974e99b98330c97a1312
remm <Hremm54@gmail.com>
2023-02-28 21:00:25 +0800

    2.28删除不必要的文件,修改书店的api,图书类型需要再做设计

--- dsk-ms-client/src/http/APIS/BookstoreApi.js
+++ dsk-ms-client/src/http/APIS/BookstoreApi.js
@@ -2,30 +2,30 @@
 import myaxios from "../MyAxios";
 
 import baseURL from "../baseURL.js";
-const bmdURL = baseURL.bmdURL;
+const dskURL = baseURL.dskURL;
 
-const cinemaApi = {
+const bookstoreApi = {
   /** 查询所有电影院 */
   queryAll() {
-    let url = bmdURL + "/cinemas";
+    let url = dskURL + "/book_stores";
     return myaxios.get(url);
   },
 
   /** 添加影院 */
   add(params) {
-    let url = bmdURL + "/cinema/add";
+    let url = dskURL + "/book_store/add";
     return myaxios.post(url, params);
   },
 
   /** 查询影院类型 */
   queryTypes() {
-    let url = bmdURL + "/cinema/tags";
+    let url = dskURL + "/book_store/types";
     return myaxios.get(url);
   },
   //删除
   del(params) {
-    let url = bmdURL + "/cinema/del";
+    let url = dskURL + "/book_store/del";
     return myaxios.post(url, params);
   },
 };
-export default cinemaApi;
+export default bookstoreApi;
--- dsk-ms-client/src/http/index.js
+++ dsk-ms-client/src/http/index.js
@@ -1,20 +1,12 @@
 //提供访问具体某接口模块对象
 import authorAPI from "./APIS/AuthorAPI";
-import directorAPI from "./APIS/directorAPI";
 import bookAPI from "./APIS/bookAPI";
 import adminApi from "./APIS/AdminApi";
-import cinemaApi from "./APIS/CinemaApi";
-import cinemaRoomApi from "./APIS/CinemaRoomApi";
-import bookThumbApi from "./APIS/BookThumbApi";
-import showingonPlanApi from "./APIS/ShowingonPlanApi";
+import bookstoreApi from "./APIS/BookstoreApi";
 const httpApi = {
   authorAPI,
   bookAPI,
-  directorAPI,
   adminApi,
-  cinemaApi,
-  cinemaRoomApi,
-  bookThumbApi,
-  showingonPlanApi,
+  bookstoreApi,
 };
 export default httpApi;
