b1b025afbea22a82ad0aae5fad0ae276de3e44b9
remm <Hremm54@gmail.com>
2023-03-04 09:41:44 +0800

    3.4小更改

--- dsk-ms-client/src/views/bookstore/BookStoreAdd.vue
+++ dsk-ms-client/src/views/bookstore/BookStoreAdd.vue
@@ -113,7 +113,7 @@ export default {
     },
     initMap() {
       AMapLoader.load({
-        key: "7bc653e04678082d08022d65625bad6e", // 申请好的Web端开发者Key，首次调用 load 时必填
+        key: "7bc653e04678082d08022d65625bad6e", // Web端开发者Key，首次调用 load 时必填
         version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
         plugins: ["AMap.Geocoder"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
       }).then((AMap) => {
